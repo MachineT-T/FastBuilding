dffc90197e85eab5927a68cd36a64d5274684498
MachineT-T <1297148061@qq.com>
2022-05-11 20:20:49 +0800

    添加鼠标控制视角功能

--- Assets/Scripts/FastBuilding/Initialization.cs
+++ Assets/Scripts/FastBuilding/Initialization.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Initialization : MonoBehaviour
 {
-    int length = 50, wide = 50;
-    GameObject[,] ground;
+    int length = 50, wide = 50;//地面的长宽
+    GameObject[,] ground;//地面GameObject
+    public GameObject MovingCamera;//相机的Target
     void Start()
     {
-        Camera.main.transform.position = new Vector3(length / 2, wide / 2, 0);
+        MovingCamera.transform.position = new Vector3(length / 2, wide / 2, 0);//将相机Target放置在地面的中间位置
+
+        //创建地面Cube
         ground = new GameObject[length, wide];
         for (int i = 0; i < length; ++i)
         {
