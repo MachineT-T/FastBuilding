fd0f260991eb96b7da85db785f6d01e5863313c8
MachineT-T <1297148061@qq.com>
2022-05-12 16:38:57 +0800

    修改鼠标控制视角功能1

--- Assets/Scripts/FastBuilding/Initialization.cs
+++ Assets/Scripts/FastBuilding/Initialization.cs
@@ -6,10 +6,14 @@ public class Initialization : MonoBehaviour
 {
     int length = 50, wide = 50;//地面的长宽
     GameObject[,] ground;//地面GameObject
-    public GameObject MovingCamera;//相机的Target
     void Start()
     {
-        MovingCamera.transform.position = new Vector3(length / 2, wide / 2, 0);//将相机Target放置在地面的中间位置
+        Camera.main.transform.position = new Vector3(length / 2, wide / 2, 0);//将相机放置在地面的中间位置
+
+        //调整相机角度使相机向下面对地面
+        float EulerX = Camera.main.transform.eulerAngles.x;
+        float EulerY = Camera.main.transform.eulerAngles.y;
+        Camera.main.transform.rotation = Quaternion.Euler(EulerX + 60, EulerY, 0);
 
         //创建地面Cube
         ground = new GameObject[length, wide];
--- Assets/Scripts/FastBuilding/MovingCamera.cs
+++ Assets/Scripts/FastBuilding/MovingCamera.cs
@@ -4,54 +4,27 @@ using UnityEngine;
 
 public class MovingCamera : MonoBehaviour
 {
-
-    //相机跟随的目标物体，一般是一个空物体
-    public Transform target;
-    private int MouseWheelSensitivity = 1; //滚轮灵敏度设置
-    private int MouseZoomMin = 0; //相机距离最小值
-    private int MouseZoomMax = 50; //相机距离最大值
-
-    private float moveSpeed = 10; //相机跟随速度（中键平移时），采用平滑模式时起作用，越大则运动越平滑
+    private int MouseWheelSensitivity = 5; //滚轮灵敏度设置
 
     private float xSpeed = 250.0f; //旋转视角时相机x轴转速
     private float ySpeed = 120.0f; //旋转视角时相机y轴转速
 
-    private int xMinLimit = -360;
-    private int xMaxLimit = 360;
+    private float EulerX = 0.0f; //存储相机的euler角
+    private float EulerY = 0.0f; //存储相机的euler角
 
-    private float x = 0.0f; //存储相机的euler角
-    private float y = 0.0f; //存储相机的euler角
-
-    private float Distance = 5; //相机和target之间的距离，因为相机的Z轴总是指向target，也就是相机z轴方向上的距离
-    private Vector3 targetOnScreenPosition; //目标的屏幕坐标，第三个值为z轴距离
     private Quaternion storeRotation; //存储相机的姿态四元数
-    private Vector3 CameraTargetPosition; //target的位置
     private Vector3 initPosition; //平移时用于存储平移的起点位置
     private Vector3 cameraX; //相机的x轴方向向量
     private Vector3 cameraY; //相机的y轴方向向量
-    private Vector3 cameraZ; //相机的z轴方向向量
 
-    private Vector3 initScreenPos; //中键刚按下时鼠标的屏幕坐标（第三个值其实没什么用）
-    private Vector3 curScreenPos; //当前鼠标的屏幕坐标（第三个值其实没什么用）
+    private Vector3 initScreenPos; //中键刚按下时鼠标的屏幕坐标
+    private Vector3 curScreenPos; //当前鼠标的屏幕坐标
     void Start()
     {
-        //这里就是设置一下初始的相机视角以及一些其他变量
-        var angles = transform.eulerAngles;
-        x = angles.x;
-        y = angles.y;
-        CameraTargetPosition = target.position;
-        storeRotation = Quaternion.Euler(x + 60, y, 0);
-        transform.rotation = storeRotation; //设置相机姿态，让镜头向下倾斜60°
-        Vector3 position = storeRotation * new Vector3(0.0F, 0.0F, -Distance) + CameraTargetPosition; //四元数表示一个旋转，四元数乘以向量相当于把向量旋转对应角度，然后加上目标物体的位置就是相机位置了
-        transform.position = storeRotation * new Vector3(0, 0, -Distance) + CameraTargetPosition; //设置相机位置
-
-        // Debug.Log("Camera x: "+transform.right);
-        // Debug.Log("Camera y: "+transform.up);
-        // Debug.Log("Camera z: "+transform.forward);
-
-        // //-------------TEST-----------------
-        // testScreenToWorldPoint();
-
+        //储存相机的旋转角以及四元数
+        EulerX = transform.eulerAngles.x;
+        EulerY = transform.eulerAngles.y;
+        storeRotation = Quaternion.Euler(EulerX, EulerY, 0);
     }
 
     void Update()
@@ -59,94 +32,42 @@ public class MovingCamera : MonoBehaviour
         //鼠标右键旋转功能
         if (Input.GetMouseButton(1))
         {
-            y += Input.GetAxis("Mouse X") * ySpeed * 0.02f;//鼠标在X轴上移动时，视角绕Y轴旋转
-            x -= Input.GetAxis("Mouse Y") * xSpeed * 0.02f;//鼠标在Y轴上移动时，视角绕X轴旋转
+            EulerY += Input.GetAxis("Mouse X") * ySpeed * 0.02f;//鼠标在X轴上移动时，视角绕Y轴旋转
+            EulerX -= Input.GetAxis("Mouse Y") * xSpeed * 0.02f;//鼠标在Y轴上移动时，视角绕X轴旋转
 
-            // Debug.Log("MouseX= " + Input.GetAxis("Mouse X"));
-            // Debug.Log("MouseY= " + Input.GetAxis("Mouse Y"));
+            //修改欧拉角改变后的四元数
+            storeRotation = Quaternion.Euler(EulerX, EulerY, 0);
 
-            //x = ClampAngle(x, xMinLimit, xMaxLimit);
-
-            storeRotation = Quaternion.Euler(x + 60, y, 0);
-            var position = storeRotation * new Vector3(0.0f, 0.0f, -Distance) + CameraTargetPosition;
-
+            //将改变后四元数应用到相机上
             transform.rotation = storeRotation;
-            transform.position = position;
         }
         else if (Input.GetAxis("Mouse ScrollWheel") != 0) //鼠标滚轮缩放功能
         {
-            if (Distance >= MouseZoomMin && Distance <= MouseZoomMax)
-            {
-                Distance -= Input.GetAxis("Mouse ScrollWheel") * MouseWheelSensitivity;
-            }
-            if (Distance < MouseZoomMin)
-            {
-                Distance = MouseZoomMin;
-            }
-            if (Distance > MouseZoomMax)
-            {
-                Distance = MouseZoomMax;
-            }
-            var rotation = transform.rotation;
-
-            transform.position = storeRotation * new Vector3(0.0F, 0.0F, -Distance) + CameraTargetPosition;
+            //获取相机Z轴方向，将相机的位置向Z轴方向位移
+            transform.position += Input.GetAxis("Mouse ScrollWheel") * MouseWheelSensitivity * transform.forward;
         }
 
-        //鼠标中键平移
+        //鼠标中键平移功能
         if (Input.GetMouseButtonDown(2))
         {
+            //保存相机的X轴、Y轴方向
             cameraX = transform.right;
             cameraY = transform.up;
-            cameraZ = transform.forward;
 
-            initScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetOnScreenPosition.z);
-            Debug.Log("downOnce");
+            //保存鼠标的初始位置
+            initScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
 
-            //targetOnScreenPosition.z为目标物体到相机xmidbuttonDownPositiony平面的法线距离
-            targetOnScreenPosition = Camera.main.WorldToScreenPoint(CameraTargetPosition);
-            initPosition = CameraTargetPosition;
+            //保存平移前相机的初始位置
+            initPosition = transform.position;
+
         }
 
         if (Input.GetMouseButton(2))
         {
-            curScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetOnScreenPosition.z);
-            //0.01这个系数是控制平移的速度，要根据相机和目标物体的distance来灵活选择
-            target.position = initPosition - 0.01f * ((curScreenPos.x - initScreenPos.x) * cameraX + (curScreenPos.y - initScreenPos.y) * cameraY);
-
-            //重新计算位置
-            Vector3 mPosition = storeRotation * new Vector3(0.0F, 0.0F, -Distance) + target.position;
-            transform.position = mPosition;
-
-            // //用这个会让相机的平移变得更平滑，但是可能在你buttonup时未使相机移动到应到的位置，导致再进行旋转与缩放操作时出现短暂抖动
-            //transform.position=Vector3.Lerp(transform.position,mPosition,Time.deltaTime*moveSpeed);
-
+            //保存鼠标当前位置
+            curScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            //通过鼠标在两个方向上的位移乘相机的X轴、Y轴方向求出相机应做出的位移
+            transform.position = initPosition - 0.01f * ((curScreenPos.x - initScreenPos.x) * cameraX + (curScreenPos.y - initScreenPos.y) * cameraY);
         }
-        if (Input.GetMouseButtonUp(2))
-        {
-            Debug.Log("upOnce");
-            //平移结束把cameraTargetPosition的位置更新一下，不然会影响缩放与旋转功能
-            CameraTargetPosition = target.position;
-        }
-
-    }
-
-    //将angle限制在min~max之间
-    static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360)
-            angle += 360;
-        if (angle > 360)
-            angle -= 360;
-        return Mathf.Clamp(angle, min, max);
-    }
-
-    void testScreenToWorldPoint()
-    {
-        //第三个坐标指的是在相机z轴指向方向上的距离
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(CameraTargetPosition);
-        Debug.Log("ScreenPoint: " + screenPoint);
-
-        // var worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(1,1,10));
-        // Debug.Log("worldPosition: "+worldPosition);
     }
 }
