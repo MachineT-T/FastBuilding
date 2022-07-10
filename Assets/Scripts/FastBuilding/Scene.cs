395cedec9873b6ad61468cfab3e5d3f316274551
MachineT-T <1297148061@qq.com>
2022-07-10 21:23:47 +0800

    小重构了一下，实现搭建模式下搭建矩形区域的方块功能
    将选择的方块列表单独放在SelectBlock中，搭建模式的代码逻辑单独放在BuildingMode中，增添了一个Scene类用于存放全局的变量内容

--- Assets/Scripts/FastBuilding/Scene.cs
+++ Assets/Scripts/FastBuilding/Scene.cs
@@ -8,22 +8,65 @@ public class Scene : MonoBehaviour
     public static int length = 50, wide = 50, height = 50;//地面的长宽高
     //用于管理整个场景每个位置的数组
     static GameObject[,,] blocks;
+    //保存对应位置上是否存在方块
+    static bool[,,] HavingBlocks;
 
+    //模式类型
+    public enum Mode { build, select };
+
+    //当前模式
+    public static Mode mode = Mode.build;
+
+    //鼠标是否在UI中
+    static bool IsInUI;
+
+    //获取场景方块信息的引用
     public static GameObject[,,] getBlocks()
     {
         return blocks;
     }
 
-    public static void setBlocks(GameObject obj)
+    //判断对应位置上是否存在方块
+    public static bool TestBlocks(int x, int y, int z)
     {
-        blocks[(int)obj.transform.position.x, (int)obj.transform.position.y, (int)obj.transform.position.z] = obj;
+        return HavingBlocks[x, y, z];
+    }
+
+    //设置对应位置上是否存在方块
+    public static void setBlocks(int x, int y, int z, bool t)
+    {
+        HavingBlocks[x, y, z] = t;
+    }
+
+    //鼠标进入按钮
+    public static void InUI()
+    {
+        IsInUI = true;
+    }
+
+    //鼠标离开按钮
+    public static void OutUI()
+    {
+        IsInUI = false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        HavingBlocks = new bool[length, height, wide];
+        for (int i = 0; i < length; ++i)
+        {
+            for (int j = 0; j < height; ++j)
+            {
+                for (int k = 0; k < wide; ++k)
+                {
+                    HavingBlocks[i, j, k] = false;
+                }
+            }
+        }
+        blocks = new GameObject[length, height, wide];
     }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/FastBuilding/SelectBlock.cs
+++ Assets/Scripts/FastBuilding/SelectBlock.cs
@@ -5,90 +5,23 @@ using UnityEditor;
 
 public class SelectBlock : MonoBehaviour
 {
-    //保存鼠标指针是否在UI按钮上，如果在UI按钮上则左键点击不进行射线检测
-    public bool IsInUI = false;
+    //用于保存选择的方块
+    static ArrayList selected = new ArrayList();
 
-    //保存选中的方块
-    Transform trans;
-    //保存按下左键的时间
-    float StartTime;
-    //保存抬起左键的时间
-    float EndTime;
-
-    //保存左键按下时的射线检测信息
-    RaycastHit StartHit;
-    //保存左线松开时的射线检测信息
-    RaycastHit EndHit;
-
-    //选中方块
-    void Select()
+    public static ArrayList getSelected()
     {
-        //如果在UI按钮上则不进行射线检测
-        if (IsInUI)
+        return selected;
+    }
+
+    //清空选择的方块列表
+    public static void ClearSelected()
+    {
+        for (int i = 0; i < selected.Count; ++i)
         {
-            return;
+            GameObject obj = (GameObject)selected[i];
+            Destroy(obj.GetComponent("ShowBoxCollider"));
         }
-        //射线检测
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
-        {
-            //如果射线碰撞的为地面则不选中
-            if (IsGround(hit.transform))
-            {
-                return;
-            }
-            //去除原方块的线框
-            if (trans != null)
-            {
-                Destroy(trans.gameObject.GetComponent("ShowBoxCollider"));
-            }
-            //保存选中的方块
-            trans = hit.transform;
-            //为选中的方块画线
-            trans.gameObject.AddComponent<ShowBoxCollider>();
-        }
-    }
-
-    //添加方块后选中添加的方块
-    public void SelectAdding(Transform transform)
-    {
-        //去除原方块的线框
-        if (trans != null)
-        {
-            Destroy(trans.gameObject.GetComponent("ShowBoxCollider"));
-        }
-        //保存选中的方块
-        trans = transform;
-        //为选中的方块画线
-        trans.gameObject.AddComponent<ShowBoxCollider>();
-    }
-
-    //判断选中的方块是否为地面
-    bool IsGround(Transform transform)
-    {
-        if (transform.gameObject.tag == "Ground")
-        {
-            return true;
-        }
-        return false;
-    }
-
-    //x轴旋转
-    public void RotateX()
-    {
-        trans.rotation *= Quaternion.AngleAxis(90, Vector3.right);
-    }
-
-    //y轴旋转
-    public void RotateY()
-    {
-        trans.rotation *= Quaternion.AngleAxis(90, Vector3.up);
-    }
-
-    //z轴旋转
-    public void RotateZ()
-    {
-        trans.rotation *= Quaternion.AngleAxis(90, Vector3.forward);
+        selected.Clear();
     }
 
     // Start is called before the first frame update
@@ -100,33 +33,20 @@ public class SelectBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //判断左键按下
-        if (Input.GetMouseButtonDown(0))
-        {
-            StartTime = Time.time;//记录按下左键时间
-        }
-
-        //判断左键抬起
-        if (Input.GetMouseButtonUp(0))
-        {
-            EndTime = Time.time;//记录左键抬起的时间
-            //如果按下左键时间间隔小于0.2秒则判定为点击
-            if (EndTime - StartTime < 0.2f)
-            {
-                Select();
-            }
-        }
-
         //判断按下delete键
         if (Input.GetKeyDown(KeyCode.Delete))
         {
             //删除对应gameobject
-            GameObject obj = trans.gameObject;
-            if (!IsGround(trans))
+            while (selected.Count != 0)
             {
+                //获取选择列表中gameobject
+                GameObject obj = (GameObject)selected[0];
+                //从列表中删除该项
+                selected.RemoveAt(0);
+                //设置该位置不存在方块
+                Scene.setBlocks((int)obj.transform.position.x, (int)obj.transform.position.y, (int)obj.transform.position.z, false);
+                //销毁此gameobject
                 Destroy(obj);
-                //令trans指向null
-                trans = null;
             }
         }
     }
